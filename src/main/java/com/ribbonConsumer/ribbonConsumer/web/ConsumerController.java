d359d080d4a0ace37b4d2a63b85cf5c6033c97c0
zengxinagji <zengxiangji@xdf.cn>
2018-06-04 09:45:53 +0800

    fix

--- src/main/java/com/ribbonConsumer/ribbonConsumer/web/ConsumerController.java
+++ src/main/java/com/ribbonConsumer/ribbonConsumer/web/ConsumerController.java
@@ -1,5 +1,6 @@
 package com.ribbonConsumer.ribbonConsumer.web;
 
+import com.ribbonConsumer.ribbonConsumer.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,8 +13,11 @@ public class ConsumerController {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    HelloService helloService;
     @RequestMapping(value = "/ribbon-consumer",method = RequestMethod.GET)
     public String helloConsumer(){
-        return  restTemplate.getForEntity("http://HELLO-SERVICE/hello",String.class).getBody();
+       // return  restTemplate.getForEntity("http://HELLO-SERVICE/hello",String.class).getBody();
+        return helloService.helloService();
     }
 }
